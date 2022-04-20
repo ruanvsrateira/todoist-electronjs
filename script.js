091e6a37e2d2ba6266d48e2d743f3df5c82edf49
ruanvsrateira <vruan85@gmail.com>
2022-04-19 22:29:09 -0300

    added link of cnd and added icon

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ const ulElement = document.querySelector("ul");
 const buttonAddTask = document.querySelector(".addButton");
 const body = document.querySelector("body");
 
+body.addEventListener("click", (e) => {
+    const el = e.target;
+
+    if(el.classList.contains("btnDel")) {
+        el.parentElement.remove();
+    }
+});
+
 body.addEventListener("keydown", (e) => {
     const el = e.key;
 
@@ -13,11 +21,28 @@ body.addEventListener("keydown", (e) => {
 })
 
 const addTask = (nome) => {
+    if(!nome) {
+        return ;
+    }
+
     const li = document.createElement("li");
     const input = document.querySelector('input');
+    const button = document.createElement("button");
 
     li.innerText = nome;
 
     ulElement.appendChild(li)
     input.value = "";
+
+    button.innerText = "-";
+    button.setAttribute("class", "btnDel")
+    button.setAttribute("onclick", "() => alert(123)")
+    li.appendChild(button);
+
+    const btnDel = document.querySelector("li button");
 }
+
+const delTask = (li) => {
+    li.remove()
+}
+
